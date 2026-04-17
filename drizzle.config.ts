9bad87c9078e8784bcc09d92cb860507468cd761
Webster Fever <webstersevenlanguages@gmail.com>
2026-04-16 23:23:56 -0400

    set up drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: databaseUrl,
+  },
+});
